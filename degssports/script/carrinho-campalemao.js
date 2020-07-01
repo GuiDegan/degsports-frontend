eed880d03d2dbe00d0cc7c3c4dfd293669d07584
GuiDegan <60679103+GuiDegan@users.noreply.github.com>
2020-06-30 21:36:15 -0300

    Add files via upload

---
+++ degssports/script/carrinho-campalemao.js
@@ -0,0 +1,48 @@
+var botao = document.getElementById("btn-adicionar-carrinho-bayern");
+botao.addEventListener('click', function() {
+
+alert("Seu item foi adicionado ao carrinho!")
+})
+
+var botao = document.getElementById("btn-adicionar-carrinho-bormonc");
+botao.addEventListener('click', function() {
+
+alert("Seu item foi adicionado ao carrinho!")
+})
+
+var botao = document.getElementById("btn-adicionar-carrinho-borussia");
+botao.addEventListener('click', function() {
+
+alert("Seu item foi adicionado ao carrinho!")
+})
+
+var botao = document.getElementById("btn-adicionar-carrinho-frankfurt");
+botao.addEventListener('click', function() {
+
+alert("Seu item foi adicionado ao carrinho!")
+})
+
+var botao = document.getElementById("btn-adicionar-carrinho-herta");
+botao.addEventListener('click', function() {
+
+alert("Seu item foi adicionado ao carrinho!")
+})
+
+var botao = document.getElementById("btn-adicionar-carrinho-rb");
+botao.addEventListener('click', function() {
+
+alert("Seu item foi adicionado ao carrinho!")
+})
+
+var botao = document.getElementById("btn-adicionar-carrinho-shalke04");
+botao.addEventListener('click', function() {
+
+alert("Seu item foi adicionado ao carrinho!")
+})
+
+var botao = document.getElementById("btn-adicionar-carrinho-wolfsburg");
+botao.addEventListener('click', function() {
+
+alert("Seu item foi adicionado ao carrinho!")
+})
+//----------------------------------------------------- Chart Camp. Alemão ---------------------------------------------------//
---
+++ degssports/script/carrinho.js
@@ -0,0 +1,48 @@
+var botao = document.getElementById("btn-adicionar-carrinho-botafogo");
+botao.addEventListener('click', function() {
+
+alert("Seu item foi adicionado ao carrinho!")
+})
+
+var botao = document.getElementById("btn-adicionar-carrinho-corinthians");
+botao.addEventListener('click', function() {
+
+alert("Seu item foi adicionado ao carrinho!")
+})
+
+var botao = document.getElementById("btn-adicionar-carrinho-flamengo");
+botao.addEventListener('click', function() {
+
+alert("Seu item foi adicionado ao carrinho!")
+})
+
+var botao = document.getElementById("btn-adicionar-carrinho-fluminense");
+botao.addEventListener('click', function() {
+
+alert("Seu item foi adicionado ao carrinho!")
+})
+
+var botao = document.getElementById("btn-adicionar-carrinho-palmeiras");
+botao.addEventListener('click', function() {
+
+alert("Seu item foi adicionado ao carrinho!")
+})
+
+var botao = document.getElementById("btn-adicionar-carrinho-santos");
+botao.addEventListener('click', function() {
+
+alert("Seu item foi adicionado ao carrinho!")
+})
+
+var botao = document.getElementById("btn-adicionar-carrinho-saopaulo");
+botao.addEventListener('click', function() {
+
+alert("Seu item foi adicionado ao carrinho!")
+})
+
+var botao = document.getElementById("btn-adicionar-carrinho-vasco");
+botao.addEventListener('click', function() {
+
+alert("Seu item foi adicionado ao carrinho!")
+})
+//----------------------------------------------------- Chart Camp. Brasileiro ---------------------------------------------------//
